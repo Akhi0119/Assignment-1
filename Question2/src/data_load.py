ad3725531fd3a8e8e399253db38e2808ee5222fe
Akhi0119 <akhilab.pegasian@gmail.com>
2025-03-03 21:51:26 -0600

    Update data_load.py

--- Question2/src/data_load.py
+++ Question2/src/data_load.py
@@ -1,1 +1,7 @@
-
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+data=pd.read_csv('/content/StudentsPerformance.csv')
+data.info('/content/StudentsPerformance.csv')
+data

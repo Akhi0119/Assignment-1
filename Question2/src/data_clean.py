0791f9022cf1dd04db0843b1ee632b32a2e53b2f
Akhi0119 <akhilab.pegasian@gmail.com>
2025-03-03 21:53:38 -0600

    Update data_clean.py

--- Question2/src/data_clean.py
+++ Question2/src/data_clean.py
@@ -1,1 +1,12 @@
-
+#Finding the null values
+null_counts = data.isnull().sum()
+print(null_counts)
+#Calculating the Average Score
+data['average score'] = (data['math score'] + data['writing score'] + data['reading score'])/3
+data.head()
+data.columns
+#Average Score
+data['average score'] = np.round(data['average score'], 2)
+data
+#Saving the data into Cleaned data folder
+data.to_csv('cleanedstudentperformance_data.csv',index=False)
